ef0b116bb2832f0d5cecf6494efbd4169553be22
davidmthompson <davidthompson25@proton.me>
2026-04-28 08:41:04 -0600

    Setup: Adding Swift project

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by david thompson on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
